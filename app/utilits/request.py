9933b965e796be1b2217959c0d1b1c26ed511045
Abduraxmonnn <abduraxmonasatullayev35@gmail.com>
2024-08-27 14:18:30 +0500

    OPtimized code

--- app/utilits/request.py
+++ app/utilits/request.py
@@ -7,10 +7,11 @@ load_dotenv()
 
 
 def login_request(data: dict) -> bool:
-    login_url = os.getenv("LOCAL_URL_LOGIN")
+    # login_url = os.getenv("LOCAL_URL_LOGIN")
+    login_url = os.getenv("SERVER_URL_LOGIN")
     r = requests.post(url=login_url, json={**data})
     
-    print(r.json())
+   #  print(r.json())
     if r.status_code == 200:
         return True
     else:
@@ -18,7 +19,8 @@ def login_request(data: dict) -> bool:
 
 
 def monitoring_tyype_request(data: dict) -> bool:
-    monitoring_url = os.getenv("LOCAL_URL_MONITORING")
+    # monitoring_url = os.getenv("LOCAL_URL_MONITORING")
+    monitoring_url = os.getenv("SERVER_URL_MONITORING")
     r = requests.post(url=monitoring_url, json={**data})
 
     if r.status_code == 200:
