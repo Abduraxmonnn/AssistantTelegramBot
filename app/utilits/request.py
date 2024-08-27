0badfeaf03df32a0e7794cd24890997fea72e14a
Abduraxmonnn <abduraxmonasatullayev35@gmail.com>
2024-08-27 14:19:14 +0500

    OPtimized code

--- app/utilits/request.py
+++ app/utilits/request.py
@@ -11,7 +11,7 @@ def login_request(data: dict) -> bool:
     login_url = os.getenv("SERVER_URL_LOGIN")
     r = requests.post(url=login_url, json={**data})
     
-   #  print(r.json())
+    # print(r.json())
     if r.status_code == 200:
         return True
     else:
